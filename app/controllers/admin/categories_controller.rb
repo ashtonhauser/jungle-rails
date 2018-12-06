21acdc0b5a04c4688a1dc67857d4421067b1ec04
Ashton Hauser <ashton.hauser777@gmail.com>
2018-12-06 19:28:39 +0000

    Ability to create category added

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,7 +3,22 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.all
   end
 
-  def create
+  def new
+    @category = Category.new
+  end
 
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: "Category Created"
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 end
